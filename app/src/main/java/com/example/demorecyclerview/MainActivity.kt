af0d5fd0d38ad46a1d41b0bef4d7459003b0de40
Will <will.marcx@gmail.com>
2019-10-30 15:17:47 -0300

    *Add getJson + Url

--- app/src/main/java/com/example/demorecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/demorecyclerview/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getJson()
+    }
+
+    fun getJson(){
+
+        val url ="https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1)"
+
     }
 }
