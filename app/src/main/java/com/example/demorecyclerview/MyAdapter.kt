929cfa1cd8843b4ec2d450b25dca96ab6d4501ed
Will <will.marcx@gmail.com>
2019-11-11 17:34:44 -0300

    *implement - MyAdapter

--- app/src/main/java/com/example/demorecyclerview/MyAdapter.kt
+++ app/src/main/java/com/example/demorecyclerview/MyAdapter.kt
@@ -1,7 +1,26 @@
 package com.example.demorecyclerview
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(val getListWeb: GetListWeb): RecyclerView.Adapter<>{
+class MyAdapter(val getListWeb: GetListWeb): RecyclerView.Adapter<MyAdapter.CustomViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.CustomViewHolder {
 
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: MyAdapter.CustomViewHolder, position: Int) {
+    }
+
+
+
+//
+//OBS:
+//
+    inner class CustomViewHolder(view: View): RecyclerView.ViewHolder(view)
 }
+
