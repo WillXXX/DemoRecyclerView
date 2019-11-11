e00fc9d6baf22524e057822b07b5fd146538b50b
Will <will.marcx@gmail.com>
2019-11-11 16:58:58 -0300

    *Users add data

--- app/src/main/java/com/example/demorecyclerview/Users.kt
+++ app/src/main/java/com/example/demorecyclerview/Users.kt
@@ -2,5 +2,5 @@ package com.example.demorecyclerview
 
 class GetListWeb(val items: List<Items>)
 
-class Items (val name: String, val id: Int, val full_name: String, val avatar_url: String)
+data class Items (val name: String, val id: Int, val full_name: String, val avatar_url: String)
 
